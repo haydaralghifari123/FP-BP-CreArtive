367551d9029599f0a6d287b63182cb5810072fef
Amalia Dewii <amaliaadk26@gmail.com>
2024-12-14 22:18:19 +0700

    update kotlin sign in, layout activity sign in

--- app/src/main/java/com/example/fp_bp/SignIn.kt
+++ app/src/main/java/com/example/fp_bp/SignIn.kt
@@ -1,7 +1,9 @@
 package com.example.fp_bp
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -9,15 +11,30 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class SignIn : AppCompatActivity() {
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_sign_in)
+
+//        icon Back masih blm bisa di klik, keluar apps terus
+//        val btnBack : Button = findViewById(R.id.btnBack5)
+//        btnBack.setOnClickListener {
+//            //onBackPressedDispatcher.onBackPressed()
+//            //finish()
+//            try {
+//                onBackPressedDispatcher.onBackPressed()
+//            } catch (e: Exception) {
+//                Log.e("SignIn", "Error in back button")
+//            }
+//        }
 
         val btnLogin : Button = findViewById(R.id.buttonLogin)
         btnLogin.setOnClickListener {
             val intent = Intent(this,HomeActivity::class.java)
             startActivity(intent)
         }
+
+
     }
 }
