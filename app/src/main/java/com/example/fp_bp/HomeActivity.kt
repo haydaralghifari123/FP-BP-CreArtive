1b40e0a0ffea7fce861cd13a7b0888c670c497fa
Amalia Dewii <amaliaadk26@gmail.com>
2024-12-14 12:00:02 +0700

    update layout kotlin home activity, layout fragment account, layout settings

--- app/src/main/java/com/example/fp_bp/HomeActivity.kt
+++ app/src/main/java/com/example/fp_bp/HomeActivity.kt
@@ -1,6 +1,8 @@
 package com.example.fp_bp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,24 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val home : ImageButton = findViewById(R.id.btnHome)
+        home.setOnClickListener {
+            val intent = Intent(this,HomeActivity::class.java)
+            startActivity(intent)
+        }
+//        blm ada search activity
+//        val search : ImageButton = findViewById(R.id.btnSearch)
+//        search.setOnClickListener {
+//            val intent = Intent(this,SearchActivity::class.java)
+//            startActivity(intent)
+//        }
+
+        // blm bisa ke fragment account
+        val account : ImageButton = findViewById(R.id.btnAccount)
+        account.setOnClickListener {
+            val intent = Intent(this,AccountFragment::class.java)
+            startActivity(intent)
         }
     }
 }
