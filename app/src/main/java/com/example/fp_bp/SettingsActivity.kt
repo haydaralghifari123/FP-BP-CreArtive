d0bbc3acfcb729c5ddfa2a211ff7e771fa71a49a
Amalia Dewii <amaliaadk26@gmail.com>
2024-12-23 13:11:25 +0700

    add intent at activity settings

--- app/src/main/java/com/example/fp_bp/SettingsActivity.kt
+++ app/src/main/java/com/example/fp_bp/SettingsActivity.kt
@@ -1,6 +1,9 @@
 package com.example.fp_bp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +14,24 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_settings)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnBack : ImageButton = findViewById(R.id.btnBack6)
+        btnBack.setOnClickListener {
+            finish()
         }
+
+        val txtEditProfile : TextView = findViewById(R.id.editProfile)
+        txtEditProfile.setOnClickListener {
+            val intent = Intent(this, Edit_Profile_Activity::class.java)
+            startActivity(intent)
+        }
+
+        val txtAbout : TextView = findViewById(R.id.about)
+        txtAbout.setOnClickListener {
+            val intent = Intent(this, AboutActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
