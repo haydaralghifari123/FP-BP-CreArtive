69b5365fa91a1930f94f2e6a24b5684962da82ad
haydaralghifari123 <haydaralghifari123@students.amikom.ac.id>
2024-12-22 23:51:56 -0800

    update button back pada activityHome.kt(fragment).

--- app/src/main/java/com/example/fp_bp/HomeActivity.kt
+++ app/src/main/java/com/example/fp_bp/HomeActivity.kt
@@ -7,30 +7,41 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 
 class HomeActivity : AppCompatActivity() {
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmenManager = supportFragmentManager
+        val fragmentTrx = fragmenManager.beginTransaction()
+        fragmentTrx.replace(R.id.fragmentContainerView, fragment)
+        fragmentTrx.commit()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_home)
 
-        val home : ImageButton = findViewById(R.id.btnHome)
-        home.setOnClickListener {
-            val intent = Intent(this,HomeActivity::class.java)
-            startActivity(intent)
-        }
-//        blm ada search activity
-//        val search : ImageButton = findViewById(R.id.btnSearch)
-//        search.setOnClickListener {
-//            val intent = Intent(this,SearchActivity::class.java)
-//            startActivity(intent)
-//        }
+        val btnHome: ImageButton = findViewById(R.id.btnHome)
+        val btnSearch: ImageButton = findViewById(R.id.btnSearch)
+        val btnAccount: ImageButton = findViewById(R.id.btnAccount)
 
-        // blm bisa ke fragment account
-        val account : ImageButton = findViewById(R.id.btnAccount)
-        account.setOnClickListener {
-            val intent = Intent(this,AccountFragment::class.java)
-            startActivity(intent)
+        //load 1
+        replaceFragment(HomeFragment())
+
+        btnHome.setOnClickListener {
+            replaceFragment(HomeFragment())
         }
+
+        btnSearch.setOnClickListener {
+            replaceFragment(SearchFragment())
+        }
+
+        btnAccount.setOnClickListener {
+            replaceFragment(AccountFragment())
+        }
+
     }
+
+
 }
