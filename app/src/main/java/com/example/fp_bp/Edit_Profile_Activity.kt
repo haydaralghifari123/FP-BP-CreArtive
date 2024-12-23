54e79ca80509e7339efc1463febf7c1f0b3e5766
Amalia Dewii <amaliaadk26@gmail.com>
2024-12-23 12:26:04 +0700

    make a activity edit profile, activity post, update layout activity about, activity home

---
+++ app/src/main/java/com/example/fp_bp/Edit_Profile_Activity.kt
@@ -0,0 +1,20 @@
+package com.example.fp_bp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Edit_Profile_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_edit_profile)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
