807e6c476678085e5935e6cd0e6d6b4e9c24f69e
Amalia Dewii <amaliaadk26@gmail.com>
2024-12-30 15:09:03 +0700

    update all files

--- app/src/main/java/com/example/fp_bp/AccountFragment.kt
+++ app/src/main/java/com/example/fp_bp/AccountFragment.kt
@@ -1,10 +1,12 @@
 package com.example.fp_bp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -35,6 +37,15 @@ class AccountFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_account, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val fab: FloatingActionButton = view.findViewById(R.id.floating_action_button)
+
+        fab.setOnClickListener {
+            val intent = Intent(requireContext(), NewPostActivity::class.java)
+        }
     }
 
     companion object {
