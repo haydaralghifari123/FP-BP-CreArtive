effb5ee00d9da8e8cf2f52327f1683851b53288a
Amalia Dewii <amaliaadk26@gmail.com>
2025-01-17 19:06:37 +0700

    update all files 11

--- app/src/main/java/com/example/fp_bp/SettingsActivity.kt
+++ app/src/main/java/com/example/fp_bp/SettingsActivity.kt
@@ -35,6 +35,7 @@ class SettingsActivity : AppCompatActivity() {
         val logOut: Button = findViewById(R.id.btnLogout)
         logOut.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
             startActivity(intent)
         }
     }
--- app/src/main/java/com/example/fp_bp/client/RetrofitClient.kt
+++ app/src/main/java/com/example/fp_bp/client/RetrofitClient.kt
@@ -5,8 +5,8 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://172.24.200.230/rest_api_fp/index.php/"
-    val url = "http://172.24.200.230/rest_api_fp/index.php/picture/"
+    private const val BASE_URL = "http://192.168.1.16/rest_api_fp/index.php/"
+    val url = "http://192.168.1.16/rest_api_fp/index.php/picture/"
 
     val instance: Api by lazy {
         val  retrofit= Retrofit.Builder()
